f61dbe2f9d8957bd5f6dd95a46bcde010fd156e2
DanailY <Danail.V.Yordanov@gmail.com>
2019-08-22 21:05:36 +0300

    Add Administration ReceiptController, Receipt All view and reset.css

---
+++ Web/StrumskaSlava.Web.ViewModels/Receipt/All/ReceiptAllViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace StrumskaSlava.Web.ViewModels.Receipt.All
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using AutoMapper;
+    using StrumskaSlava.Services.Data;
+    using StrumskaSlava.Services.Mapping;
+
+    public class ReceiptAllViewModel : IMapTo<ReceiptServiceModel>, IHaveCustomMappings
+    {
+        public string Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public decimal Total { get; set; }
+
+        public int Products { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<ReceiptServiceModel, ReceiptAllViewModel>()
+                .ForMember(destination => destination.Total,
+                    opts => opts.MapFrom(origin => origin.Orders.Sum(order => order.Product.Price * order.Quantity)))
+                .ForMember(destination => destination.Products,
+                    opts => opts.MapFrom(origin => origin.Orders.Sum(order => order.Quantity)));
+        }
+    }
+}
---
+++ Web/StrumskaSlava.Web/Areas/Administration/Controllers/ReceiptController.cs
@@ -0,0 +1,38 @@
+﻿namespace StrumskaSlava.Web.Areas.Administration.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using StrumskaSlava.Services;
+    using StrumskaSlava.Services.Data;
+    using StrumskaSlava.Services.Mapping;
+    using StrumskaSlava.Web.ViewModels.Receipt.All;
+
+    public class ReceiptController : AdministrationController
+    {
+        private readonly IReceiptService receiptService;
+
+        public ReceiptController(IReceiptService receiptService)
+        {
+            this.receiptService = receiptService;
+        }
+
+        [HttpGet]
+        [Route("/Administration/Receipt/All")]
+        public async Task<IActionResult> All()
+        {
+
+            List<ReceiptServiceModel> receiptsFromDb = await this.receiptService
+                .GetAll().ToListAsync();
+
+            List<ReceiptAllViewModel> receiptsForCurrentUser = receiptsFromDb
+                .Select(receipt => receipt.To<ReceiptAllViewModel>()).ToList();
+
+            return this.View(receiptsForCurrentUser);
+        }
+    }
+}
